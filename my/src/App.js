f2efb48e2539eb960bbf5ca0cd41eb652cae72a4
Karimullah <59876062+KarimullahOfficial@users.noreply.github.com>
2023-09-21 16:11:03 +0500

    p

---
+++ my/src/App.js
@@ -0,0 +1,14 @@
+import First from "./Component/First";
+
+const App = () => {
+  return (
+    <>
+      <div className="App">
+        <First></First>
+      </div>
+      ;
+    </>
+  );
+};
+
+export default App;
---
+++ my/src/Component/First.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const First = () => {
+  return (
+    <>
+    
+    <h1>this is function one</h1> 
+    </>
+  )
+}
+
+export default First
